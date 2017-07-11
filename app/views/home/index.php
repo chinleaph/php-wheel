8e6d80d027fbe1f4c2be31e398eefa1c0f04cc57
chin <lethetime@163.com>
2017-07-12 00:45:51 +0800

    顶部导航

---
+++ app/views/home/index.php
@@ -0,0 +1,29 @@
+<style>
+    .nav li a {height:50px;border: none;}
+    .logo a img{height: 50px;}
+</style>
+<div class="container">
+    <ul class="nav nav-secondary nav-justified text-lg">
+        <li >
+            <div class="logo">
+            <a href="/home/index">
+                <img class="img-circle" data-toggle="tooltip" data-placement="bottom" title="首页" src="http://pic.58pic.com/58pic/11/39/34/10D58PICHzu.jpg">
+            </a>
+            </div>
+        </li>
+        <li><a href="your/nice/url">首页</a></li>
+        <li><a href="your/nice/url">动态 <span class="label label-badge label-success">4</span></a></li>
+        <li><a href="">关于</a></li>
+        <li><a href="">关于</a></li>
+        <li><a href="">关于</a></li>
+        <li><a href="">关于</a></li>
+        <li><a href="">关于</a></li>
+
+    </ul>
+</div>
+<script>
+    jQuery(function () {
+        // 你需要手动初始化工具提示
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
